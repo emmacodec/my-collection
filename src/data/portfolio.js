559aeabb7217cfa8a48bcf937b0daf187bd98e42
Emmacodec <danielemmanuel7553@gmail.com>
2023-06-21 03:38:19 +0100

    update

---
+++ src/data/portfolio.js
@@ -0,0 +1,22 @@
+ export const portfolio = [
+    {
+        title: 'Terminal website',
+        imgUrl: '/assets/img3',
+        stack: ['HTML, CSS, Javascript'],
+        link: 'https://dan portfolio.com',
+    },
+
+    {
+        title: 'Developer Portfolio',
+        imgUrl: '/assets/img1/img1-1',
+        stack: ['React, Tailwind, Vite'],
+        link: 'https://dan collection.com',
+    },
+
+    {
+        title: 'Alien Ware',
+        imgUrl: '/assets/img2',
+        stack: ['Python, C++, Robotics'],
+        link: 'https://github.com/emmacodec',
+    },
+]
