3074605f89586f77cfbce1aa2e0076c333e40e15
Emmacodec <danielemmanuel7553@gmail.com>
2023-06-23 16:03:31 +0100

    update

--- src/components/timelineitem.jsx
+++ src/components/timelineitem.jsx
@@ -6,7 +6,7 @@ const Timelineitem = (props) => {
   return (
     <ol className="flex flex-col md:flex-row relative border-l border-stone-200">
       <li className="mb-10 ml-4">
-        <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-white" />
+        <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-black" />
         <p className="flex flew-wrap gap-4 flex-row items-center justify-start text-xm md:text-sm">
             <span className="inline-block px-2 py-1 font-semibold text-white bg-stone-900 rounded-md">
                 {year}
@@ -14,7 +14,7 @@ const Timelineitem = (props) => {
                 <h3 className="text-lg font-semibold text-stone-900">
                     {title}
                 </h3>
-                <div className="my-1 text-sm- font-normal leading-none text-stone-400">
+                <div className="my-1 text-sm font-bold leading-none text-stone-400">
                     {duration}
                 </div>
         </p>
